55a319c5808e0ae3108fcee89ac24fe49a3df61f
zfqdinesh <mynamedineshajmera@gmail.com>
2025-07-13 22:37:09 +0530

    added some more websit code

--- app.py
+++ app.py
@@ -6,6 +6,33 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/youtube")
+def youtube():
+    return render_template("youtube.html")
+
+@app.route("/netflix")
+def netflix():
+    return render_template("netflix.html")
+
+@app.route("/love")
+def love():
+    return render_template("love.html")
+
+@app.route("/jio_mart")
+def smart_mart():
+    return render_template("smart_mart.html")
+
+@app.route("/fillif")
+def fitlif():
+    return render_template("fitlif.html")
+
+@app.route("/health")
+def health():
+    return render_template("health.html")
+
+
+
+
 
 
 
